dcc29fe07253dac3556a296cba9d80c9d6a5751a
silence <mahuateng@tedu.cn>
2019-10-17 15:50:52 +0800

    跟新注册功能代码

--- user-service/src/main/java/com/programmerhub/user/controller/UserController.java
+++ user-service/src/main/java/com/programmerhub/user/controller/UserController.java
@@ -15,7 +15,31 @@ import com.programmerhub.user.service.UserService;
 import redis.clients.jedis.JedisCluster;
 
 @RestController
-@RequestMapping("")
-public class UserController{
-
+@RequestMapping
+public class UserController {
+	@Autowired
+	private UserService userService;
+	//注册时校验用户名重复
+	@RequestMapping("checkUserName")
+	public SysResult checkUserName(String userName){
+		//空层判断查询结果可用不可用
+		int exists=userService.checkUserExists(userName);
+		//1/0 1-->不可用-->201 0-->可用-->200
+		if(exists==1){
+			return SysResult.build(201, "不可用", null);
+		}else{
+			return SysResult.ok();
+		}
+	}
+	//注册表单提交，user对象接收数据
+	@RequestMapping("register")
+	public SysResult doRegister(User user){
+		try{ 
+			userService.doRegister(user);
+			return SysResult.ok();
+		}catch(Exception e){
+			e.printStackTrace();
+			return SysResult.build(201, "注册失败", null);
+		}
+	}
 }
--- user-service/src/main/java/com/programmerhub/user/mapper/UserMapper.java
+++ user-service/src/main/java/com/programmerhub/user/mapper/UserMapper.java
@@ -1,5 +1,11 @@
 package com.programmerhub.user.mapper;
 
+import com.programmerhub.common.pojo.User;
+
 public interface UserMapper {
+
+	int selectUserCountByUserName(String userName);
+
+	void insertUser(User user);
 
 }
