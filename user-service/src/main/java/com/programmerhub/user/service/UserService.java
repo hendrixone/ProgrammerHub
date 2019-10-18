c3ac8bd8ce78a0e82a76562b2bbc441aa09f34ed
hendrixwan <hendrixwan@gmail.com>
2019-10-18 14:47:46 +0800

    完成用户注册功能

--- user-service/src/main/java/com/programmerhub/user/service/UserService.java
+++ user-service/src/main/java/com/programmerhub/user/service/UserService.java
@@ -20,18 +20,17 @@ public class UserService {
 	@Autowired
 	private UserMapper userMapper;
 	public void doRegister(User user) throws Exception {	
-		if(userMapper.selectUserByEmail(user.getEmail()) != null){
+		if(userMapper.selectUserByEmail(user.getUserEmail()) != null){
 			throw new Exception("用户已存在");
 		}
 		//对密码进行加密
 		//对密码加密方式 安全可以使用md5加盐
 		//user.getUserPassword
-		user.setPassword
-		(MD5Util.md5(user.getPassword()));
+		user.setUserPassword(MD5Util.md5(user.getUserPassword()));
 		//添加邮箱并加密
 		//使用md5加盐方式加密
 		//user.getEmail
-		user.setEmail(MD5Util.md5(user.getEmail()));
+		user.setUserEmail(user.getUserEmail());
 		userMapper.insertUser(user);
 	}
 	@Autowired(required=false)
